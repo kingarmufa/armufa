6eff3968fa44eec4acf786d4fa5f31dc577bdb62
kingarmufa <armufastore@gmail.com>
2026-07-16 13:21:21 +0700

    Uploaded Successfully

---
+++ setting.js
@@ -0,0 +1,38 @@
+// Konfigurasi untuk Bot Telegram
+global.botToken = '8382042800:AAGunSWHiqZKSbp8nTS7kutMiSSxJzbEYps'; // <-- GANTI DENGAN TOKEN DARI BOTFATHER
+global.ownerIds = ['6854552378', '6854552378']; // <-- GANTI DENGAN ID TELEGRAM OWNER BOT, bisa lebih dari 1
+
+// Konfigurasi Global Lainnya
+global.author = 'ARMUFA CPANEL';
+
+// =================== KONFIGURASI MULTI-PANEL ===================
+global.panels = {
+    "panel1": {
+        name: "Panel 1",
+        url: 'https://',
+        ptla: 'ptla_',
+        ptlc: 'ptlc_', // <-- PENTING: Client API Key, diperlukan untuk mode hapus 'inactive'
+        egg: '15',
+        nest: '5',
+        loc: '1'
+    },
+    "panel2": {
+        name: "Panel 2",
+        url: 'https://',
+        ptla: 'ptla_',
+        ptlc: 'ptlc_',// <-- PENTING: Client API Key, diperlukan untuk mode hapus 'inactive'
+        egg: '15',
+        nest: '5',
+        loc: '1'
+    },
+    "panel3": {
+        name: "Panel 3",
+        url: 'https:/',
+        ptla: 'ptla_',
+        ptlc: 'ptlc_', // <-- PENTING: Client API Key, diperlukan untuk mode hapus 'inactive'
+        egg: '15',
+        nest: '5',
+        loc: '1'
+    }
+};
+// ===============================================================
